3edc79766010cdb6326bd7adefa56103817b26e1
Wiwie Sanjaya <wiwiewei18@gmail.com>
2024-09-04 16:03:40 +0700

    feat: add image url validationn

--- utils/image_classifier.py
+++ utils/image_classifier.py
@@ -1,7 +1,9 @@
 from transformers import pipeline
 import numpy as np
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from pathlib import Path
+import requests
+from io import BytesIO
 
 
 def classify(image_url):
@@ -26,5 +28,26 @@ def preprocess_image(image):
         image = Image.open(image)
     elif isinstance(image, np.ndarray):
         image = Image.fromarray(image)
+
+    return image
+
+
+def validate_image_url(image_url):
+    if image_url is None:
+        raise ValueError("Input image cannot be None.")
+
+    try:
+        if isinstance(image_url, str) or isinstance(image_url, Path):
+            if str(image_url).startswith(("http://", "https://")):
+                response = requests.get(image_url)
+                response.raise_for_status()
+                image = Image.open(BytesIO(response.content))
+            else:
+                image = Image.open(image_url)
+
+    except (FileNotFoundError, UnidentifiedImageError) as err:
+        raise ValueError(f"Unable to load the image: {err}")
+    except requests.exceptions.RequestException as err:
+        raise ValueError(f"Unable to access the URL: {err}")
 
     return image
